ca944fa3df392b27425268b8999977c457824abb
Bohdanok <bohdan.milian@gmail.com>
2024-10-13 20:32:14 +0300

    working tests

--- src/main/java/flower/store/Main.java
+++ src/main/java/flower/store/Main.java
@@ -1,19 +1,23 @@
 package flower.store;
 
 public class Main {
+    private static final int TEN = 10;
+    private static final int EIGHTY = 80;  // Fixed typo from EIGHTTY to EIGHTY
+    private static final int HUNDRED = 100;
+    private static final int EIGHT = 8;
+
     public static void main(String[] args) {
-        int TEN = 10;
-        int EIGHTTY = 80;
-        int HUNDRED = 100;
-        int EIGHT = 8;
-        Flower rose = new Flower(FlowerColor.RED, EIGHTTY, HUNDRED, FlowerType.ROSE);
+        Flower rose = new Flower(FlowerColor.RED, EIGHTY, HUNDRED, FlowerType.ROSE);
         FlowerPack flowerPack1 = new FlowerPack(rose, TEN);
         FlowerPack flowerPack2 = new FlowerPack(rose, EIGHT);
+        
         FlowerBucket flowerBucket = new FlowerBucket();
         flowerBucket.add(flowerPack1);
         flowerBucket.add(flowerPack2);
+
         System.out.println(flowerBucket.getPrice());
-        rose.setPrice(TEN);
+        
+        rose.setPrice(TEN);  // This line is fine if `setPrice` accepts an int.
         System.out.println(flowerBucket.getPrice());
     }
 }
